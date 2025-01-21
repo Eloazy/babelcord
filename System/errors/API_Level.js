9944f3fd320da7f1b1a607c971c15b6b888e6445
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-21 10:32:12 -0300

    Delete System directory

--- System/errors/API_Level.js
+++
@@ -1,4 +0,0 @@
-// disabled - any command redirect to this JS
-module.exports = async function(interaction) {
-	interaction.reply('your key level is not hight enought')
-}
--- System/useful/fetchData.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(link) {
-	var Data = null
-	await fetch(link).then(response => {
-		if(!response.ok) {return -1}
-		return response.json()
-	}).then(data => {Data = data})
-	return Data
-}
