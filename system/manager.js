d6e54b85f529db7d07e2db9dfac2d3537bd2ca70
Yume <149901413+Eloazy@users.noreply.github.com>
2025-02-01 15:22:48 -0300

    update v4
    deleted integration, spotify and torn commands, keep AI call but now request are made by a Server and not more internal by bot

--- system/manager.js
+++ system/manager.js
@@ -1,53 +1,7 @@
-//integrated
-const info = require('./integrated/info.js')
-const random = require('./integrated/randomNumber.js')
-//torncity
-const t_info = require('./tornCity/info.js')
-//user
-const t_profile = require('./tornCity/user/profile.js')
-const t_money = require('./tornCity/user/money.js')
-const t_revives = require('./tornCity/user/revive.js')
-//faction
-const f_basic = require('./tornCity/faction/basic.js')
-//useful
-const us_investment = require('./tornCity/useful/investment.js')
-//spotfy
-const s_info = require('./spotify/info.js')
-const s_search = require('./spotify/search.js')
-//babelaife
-const b_info = require('./babelaife/info.js')
-const b_babelaife = require('./babelaife/babelaife.js').default
+import b_babelaife from './babelaife/babelaife.js'
 
-module.exports = function(interaction, client) {
-	if(interaction.commandName === "integrated") {
-		if(interaction.options._subcommand === "info") {info(interaction, client)}
-		if(interaction.options._subcommand === "random") {random(interaction, interaction.options.getNumber('max') ?? 2)}
-	}
-	if(interaction.commandName === "spotify") {
-		if(interaction.options._subcommand === "info") {s_info(interaction)}
-		if(interaction.options._subcommand === "search") {s_search(interaction, interaction.options.getString('query'))}
-	}
+export default function(interaction, client) {
 	if(interaction.commandName === "babelaife") {
-		if(interaction.options._subcommand === "info") {b_info(interaction)}
-		if(interaction.options._subcommand === "chat") {b_babelaife(interaction, interaction.options.getString('query'))}
-	}
-	if(interaction.commandName === "torncity") {
-		if(interaction.options._subcommand === "info") {t_info(interaction)}
-		
-		if(interaction.options._group === "user") {
-			if(interaction.options._subcommand === "profile") {t_profile(interaction, interaction.options.getNumber("id") ?? 3028393)}
-			if(interaction.options._subcommand === "money") {t_money(interaction, 3028393, interaction.options.getBoolean("ephemeral") ?? true)}
-				if(interaction.options._subcommand === "revives") {t_revives(interaction, 3028393)}
-		}
-		if(interaction.options._group === "faction") {
-			if(interaction.options._subcommand === "basic") {
-				f_basic(interaction, interaction.options.getNumber('id') ?? 53051)
-			}
-		}
-		if(interaction.options._group === "useful") {
-			if(interaction.options._subcommand === "investment") {
-				us_investment(interaction, interaction.options.getNumber('value'), interaction.options.getNumber('percentage'))
-			}
-		}
+		b_babelaife(interaction, interaction.options.getString('query'))
 	}
 }
