d7dd6d3d89ea601da0afcbe496886e3dffa72b14
Yume <149901413+Eloazy@users.noreply.github.com>
2025-02-01 15:23:47 -0300

    update v4
    deleted integration, spotify and torn commands, keep AI call but now request are made by a Server and not more internal by bot

--- system/babelaife/babelaife.js
+++ system/babelaife/babelaife.js
@@ -1,58 +1,16 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-const genAI = new GoogleGenerativeAI("AIzaSyBIF4mjYEk-VtQZpXIIoV_wtd49iLzPLFY");
-const model = genAI.getGenerativeModel({ 
-	model: "gemini-2.0-flash-thinking-exp-01-21",
-	systemInstruction: {
-		parts: [
-			{text: 'you name is Babel'},
-			{text: 'The response text must not exceed 2000 characters'},
-		]
-	},
-	generationConfig: {
-		maxOutputTokens:2000,
-		temperature: 0,
-		top_p:0
-	}
-});
-
-let chat = model.startChat({
-	history: []
-})
-
-function resetChatHistory() {chat = model.startChat({ history: [] })}
-
 export default async function(interaction, query) {
 	await interaction.deferReply()
-	if(query.toLowerCase() === "clean_historic") {
-		resetChatHistory()
-		return interaction.editReply("clean historic")
-	}
-	const result = await chat.sendMessage(query);
-	var time = new Date().getTime();
-
-	await model.generateContent({
-		contents: [{ parts: [{ text: query }] }],
-		safetySettings: [ 
-		    {
-		        category: "HARM_CATEGORY_HARASSMENT",
-		        threshold: "BLOCK_NONE"
-		    },
-		    {
-		        category: "HARM_CATEGORY_HATE_SPEECH",
-		        threshold: "BLOCK_NONE"
-		    },
-		    {
-		        category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-		        threshold: "BLOCK_NONE"
-		    },
-		    {
-		        category: "HARM_CATEGORY_DANGEROUS_CONTENT",
-		        threshold: "BLOCK_NONE"
-		    }
-	    ]
-	}).then((res) => {
-		time = new Date().getTime() - time
-		interaction.editReply('> '+query+'\n'+result.response.text()+`\n-# Reply in ${time} milliseconds`)
-	})
+	const response = await fetch('http://localhost:5000/api/babelaife', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify({ query })
+    })
+    if(response.ok) {
+    	const data = await response.json()
+    	await interaction.editReply(data.result)
+    }
+    else {console.log('error: ', response.status)}
 }
+
