4259bfcc295999e230733f917999e5c2dc1760ee
Yume <149901413+Eloazy@users.noreply.github.com>
2025-02-16 00:41:04 -0300

    Add files via upload

--- system/manager.js
+++ system/manager.js
@@ -2,6 +2,6 @@ import b_babelaife from './babelaife/babelaife.js'
 
 export default function(interaction, client) {
 	if(interaction.commandName === "babelaife") {
-		b_babelaife(interaction, interaction.options.getString('query'))
+		b_babelaife(interaction, interaction.options.getString('query') + ' (answer simply)')
 	}
 }
