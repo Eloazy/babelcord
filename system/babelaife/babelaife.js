d8998294fdc45e053270a59ff72b7c1aa02824e9
Yume <149901413+Eloazy@users.noreply.github.com>
2025-02-16 00:41:20 -0300

    Add files via upload

--- system/babelaife/babelaife.js
+++ system/babelaife/babelaife.js
@@ -1,16 +1,19 @@
 export default async function(interaction, query) {
-	await interaction.deferReply()
-	const response = await fetch('http://localhost:5000/api/babelaife', {
-        method: 'POST',
-        headers: {
-            'Content-Type':'application/json',
-        },
-        body: JSON.stringify({ query })
-    })
-    if(response.ok) {
-    	const data = await response.json()
-    	await interaction.editReply(data.result)
-    }
-    else {console.log('error: ', response.status)}
+	//return interaction.reply('unavaliable | incompatible with BabelServer 2.0')
+    await interaction.deferReply()
+	try {
+        const response = await fetch('http://localhost:1025/api/babelaife', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({ query })
+        })
+        if(!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)}
+        
+        const data = await response.json()
+        if(!data) {throw new Error(`Error: undefined data`)}
+        await interaction.editReply(data.result.result || data.result)
+    } catch(err) {interaction.editReply(`discord extension error: ${err}`)}
 }
 
